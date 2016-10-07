7c533459e07d9b194049025d909e58849ddd693a
Dami <dami@creabox.net>
2016-10-08 00:58:45 +0200

    Test AE on linux

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "Coconut",
     dependencies: [
 	.Package(url: "https://github.com/damicreabox/CGtk3.git", majorVersion: 1),
-	.Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 3)
+	.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4)
     ]
 )
--- Sources/Coconut-converter/main.swift
+++ Sources/Coconut-converter/main.swift
@@ -7,16 +7,17 @@
 //
 
 import Foundation
-import SWXMLHash
+import AEXML
 
 let xmlToParse = URL(fileURLWithPath: "Files/Converter/Nibs/MainMenu.xib")
 let data = try? Data(contentsOf: xmlToParse)
-let xml = SWXMLHash.parse(data!)
 
+let xmlDoc = try AEXMLDocument(xml: data!)
 
-do {
-    let node = try xml.byKey("document").byKey("dependencies")
-    print("\(node.description)")
-} catch {
-    print("Error : \(error)")
+// prints the same XML structure as original
+print(xmlDoc.xml)
+
+// prints cats, dogs
+for child in xmlDoc.root.children {
+    print(child.name)
 }
